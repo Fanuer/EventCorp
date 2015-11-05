bf701e2e0001898b4dbc2eb57f672a4900b5e20c
Fanuer <SteSue@web.de>
2015-11-05 07:19:36 +0100

    initial commit 2

---
+++ EventCorp.AuthorizationServer/Models/ModelFactory.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Routing;
+using EventCorp.AuthorizationServer.Entites;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EventCorp.AuthorizationServer.Models
+{
+    public class ModelFactory
+    {
+        #region Field
+        private readonly UrlHelper _UrlHelper;
+        #endregion
+
+        #region Ctor
+        public ModelFactory(HttpRequestMessage request)
+        {
+            _UrlHelper = new UrlHelper(request);
+        }
+        #endregion
+
+        #region Method
+        public UserModel CreateViewModel(User datamodel)
+        {
+            if (datamodel == null) { throw new ArgumentNullException("datamodel"); }
+            return new UserModel
+            {
+                Url = _UrlHelper.Link("GetUserById", new { id = datamodel.Id }),
+                Id = datamodel.Id,
+                UserName = datamodel.UserName,
+                Email = datamodel.Email,
+                DateOfBirth = datamodel.DateOfBirth,
+                Surname = datamodel.Surname,
+                Forename = datamodel.Forename
+            };
+        }
+
+        public RoleModel CreateViewModel(IdentityRole datamodel)
+        {
+            if (datamodel == null)
+            {
+                throw new ArgumentNullException("datamodel");
+            }
+            return new RoleModel
+            {
+                Url = _UrlHelper.Link("GetRoleById", new { id = datamodel.Id }),
+                Id = datamodel.Id,
+                Name = datamodel.Name
+            };
+        }
+
+        public RefreshTokenModel CreateViewModel(RefreshToken datamodel)
+        {
+            if (datamodel == null)
+            {
+                throw new ArgumentNullException("datamodel");
+            }
+
+            return new RefreshTokenModel()
+            {
+                Id = datamodel.Id,
+                ClientId = datamodel.ClientId,
+                Subject = datamodel.Subject,
+                ExpiresUtc = datamodel.ExpiresUtc,
+                IssuedUtc = datamodel.IssuedUtc
+            };
+        }
+
+        internal User CreateModel(UserModel model, User datamodel = null)
+        {
+            var result = datamodel ?? new User();
+            result.DateOfBirth = model.DateOfBirth;
+            result.UserName = model.UserName;
+            result.Email = model.Email;
+            result.Forename = model.Forename;
+            result.Surname = model.Surname;
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ EventCorp.AuthorizationServer/Providers/CustomRefreshTokenProvider.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventCorp.AuthorizationServer.Entites;
+using EventCorp.AuthorizationServer.Interfaces.Repositories;
+using EventCorp.AuthorizationServer.Repository;
+using EventCorps.Helper;
+using Microsoft.Owin.Security.Infrastructure;
+
+namespace EventCorp.AuthorizationServer.Providers
+{
+    public class CustomRefreshTokenProvider : IAuthenticationTokenProvider
+    {
+        private const string IS_REFREHTOKEN_EXPIRED_NAME = "IsRefreshTokenExpired";
+
+        public void Create(AuthenticationTokenCreateContext context)
+        {
+            this.CreateAsync(context).Wait();
+        }
+
+        public async Task CreateAsync(AuthenticationTokenCreateContext context)
+        {
+            if (!context.OwinContext.Environment.ContainsKey(IS_REFREHTOKEN_EXPIRED_NAME) || (bool)context.OwinContext.Environment[IS_REFREHTOKEN_EXPIRED_NAME])
+            {
+                bool result = false;
+                var refreshTokenId = Guid.NewGuid().ToString("n");
+                var clientId = context.Ticket.Properties.Dictionary["as:client_id"];
+
+                var refreshTokenLifetime = context.OwinContext.Get<string>("as:clientRefreshTokenLifeTime") ?? "30";
+                var token = new RefreshToken()
+                {
+                    Id = Utilities.GetHash(refreshTokenId),
+                    ClientId = clientId,
+                    Subject = context.Ticket.Identity.Name,
+                    IssuedUtc = DateTime.UtcNow,
+                    ExpiresUtc = DateTime.UtcNow.AddDays(Double.Parse(refreshTokenLifetime))
+                };
+                context.Ticket.Properties.IssuedUtc = token.IssuedUtc;
+                context.Ticket.Properties.ExpiresUtc = token.ExpiresUtc;
+
+                token.ProtectedTicket = context.SerializeTicket();
+
+                using (IApplicationRepository rep = new ApplicationRepository())
+                {
+                    result = await rep.RefreshTokens.AddAsync(token);
+                }
+                if (result)
+                {
+                    context.SetToken(refreshTokenId);
+                }
+            }
+        }
+
+        public void Receive(AuthenticationTokenReceiveContext context)
+        {
+            this.ReceiveAsync(context).Wait();
+        }
+
+        public async Task ReceiveAsync(AuthenticationTokenReceiveContext context)
+        {
+            try
+            {
+                var allowedOrigin = context.OwinContext.Get<string>("as:clientAllowedOrigin") ?? "*";
+                context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { allowedOrigin });
+
+                var hashedTokenId = Utilities.GetHash(context.Token);
+                using (IApplicationRepository rep = new ApplicationRepository())
+                {
+                    var refreshToken = await rep.RefreshTokens.FindAsync(hashedTokenId);
+
+                    if (refreshToken != null)
+                    {
+                        //Get protectedTicket from refreshToken class
+                        context.DeserializeTicket(refreshToken.ProtectedTicket);
+                        var result = await rep.RefreshTokens.RemoveAsync(hashedTokenId);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+    }
+}
