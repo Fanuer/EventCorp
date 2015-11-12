5e9d0a87140885e4758b29351743f29544d6d7b7
Kevin Schie <kevin.schie@me.com>
2015-11-12 11:51:50 +0100

    AngularJS hinzugefügt + bower_components

---
+++ Site/app/app.js
@@ -0,0 +1,24 @@
+﻿var eventCorp = angular.module("eventCorp", ['ngRoute']);
+
+eventCorp.config([
+    "$routeProvider", function($routeProvider) {
+        $routeProvider.when("/", {
+            controller: "dashboardController",
+            templateUrl: "app/views/dashboard.html"
+        }).when("/events", {
+            controller: "eventController",
+            templateUrl: "app/views/events.html"
+        }).when("/register", {
+            controller: "registerController",
+            templateUrl: "app/views/register.html"
+        }).when("/user", {
+            controller: "userController",
+            templateUrl: "app/views/user.html"
+        }).when("/login", {
+            controller: "loginController",
+            templateUrl: "app/views/login.html"
+        }).otherwise("/");
+    }
+]);
+
+eventCorp.controller("dashboardController", dashboardController);
